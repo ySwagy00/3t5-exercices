4203fa5f41e28cd8318ade5ebb45e7fe99a127e2
vcarrier <vincent.carrier@cegepmontpetit.ca>
2025-08-25 22:57:26 -0400

    Exercices R02

---
+++ R02 - Cmdlets et objets/exercice2.2.ps1
@@ -0,0 +1,47 @@
+﻿# Exercice 2: Commandes PowerShell
+
+# Trouvez la commande PowerShell et leurs arguments permettant de faire ce qui est demandé.
+
+# Utilisez la vraie commande PowerShell, pas un alias ni une commande externe.
+
+
+
+# 1. Obtenir la liste des fichiers et répertoires contenus dans le répertoir courant (équivalent à dir).
+
+
+# 2. Obtenir la liste des fichiers seulement contenus directement sous C:\Windows.
+
+
+# 3. Obtenir la liste des répertoires seulement contenus directement sous C:\Program Files.
+
+
+# 4. Obtenir la liste des processus actifs dont le nom commence par "V" (vous pouvez utiliser les wildcards).
+
+
+# 5. Obtenir de l'information sur le volume C: (comme sa taille, l'espace restant, le format du système de fichiers, etc.)
+
+
+# 6. Créer un nouveau répertoire nommé Minou à la racine du volume C:\.
+
+
+# 7. Définir ce répertoire comme répertoire de travail (répertoire de travail, équivalent à cd C:\Minou).
+
+
+# 8. Obtenir la liste des adapteurs réseau installés sur l'ordinateur.
+
+
+# 9. Obtenir la liste des utilisateurs locaux configurés sur l'ordinateur.
+
+
+# 10. Obtenir l'adresse IP numérique du serveur www.cegepmontpetit.ca.
+
+
+# 11. Vérifier si le serveur www.cegepmontpetit.ca répond sur le port 443 à l'aide de Test-NetConnection.
+
+
+# 12. Obtenez la date (et l'heure) de maintenant.
+
+
+# 13. <DÉFI> Obtenez la date et l'heure dans le format suivant: 2025-08-26 14:55. Consultez l'aide en ligne (Get-Help -Online) pour plus de détails sur l'utilisation de cette commande.
+
+
---
+++ R02 - Cmdlets et objets/exercice2.3.ps1
@@ -0,0 +1,60 @@
+﻿# Exercice 3: Objets
+
+# Dans cet exercice, trouvez la ligne de commande PowerShell à utiliser pour obtenir l'information demandée. Les 
+# questions suivantes doivent être répondue en utilisant les objets.
+
+
+################################
+# Partie 1: Système de fichiers
+################################
+
+# 1. Stockez dans la variable $ipconfig un objet représentant le fichier C:\Windows\System32\ipconfig.exe (avec Get-Item).
+
+
+# 2. Obtenez la liste de toutes les propriétés et méthodes offertes par cet objet (avec Get-Member).
+
+
+# 3. À l'aide de la commande Select-Object, obtenez le maximum d'informations contenu dans cet objet.
+
+
+# 4. Affichez à l'écran le nom complet de ce fichier (C:\Windows\System32\ipconfig.exe), en utilisant les propriétés de cet objet.
+
+
+# 5. Affichez seulement le nom de l'exécutable (ipconfig.exe).
+
+
+# 6. Affichez seulement le nom du répertoire dans lequel il se trouve (C:\Windows\System32).
+
+
+# 7. Affichez la taille de ce fichier, en octets.
+
+
+# 8. Affichez la date de création de ce fichier.
+
+
+# 9. <DÉFI> Affichez la date de création de ce fichier en format yyyy-MM-dd. (Vous aurez besoin de Get-Date).
+
+
+# 10. <DÉFI> Affichez le nombre de jours qui se sont écoulés depuis la création de ce fichier. (Vous aurez besoin de New-TimeSpan).
+
+
+##############################
+# Partie 2: Objets PSCulture
+##############################
+
+# 1. Stockez l'objet produit par Get-Culture dans une variable nommée $Culture.
+
+
+# 2. Obtenez la liste de toutes les propriétés et méthodes offertes par cet objet
+
+
+# 3. À l'aide de la commande Select-Object, obtenez le maximum d'informations contenu dans cet objet.
+
+
+# 4. Affichez dans la console seulement la propriété DisplayName.
+
+
+# 5. Dans cet objet se trouve un sous-objet représentant le format des nombres dans cette langue, dans lequel se trouve le symbole de la device monétaire (currency, soit $). Affichez ce symbole dans la console.
+
+
+# 6. Obtenez la date du jour dans le format de date courte de la langue courante.
